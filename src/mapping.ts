23b52c1fce2cf781ab91f165e7543ea117273c64
Rami Husami <husamirami@gmail.com>
2023-02-27 11:40:04 +0300

    add liquid staking subgraph

---
+++ src/mapping.ts
@@ -0,0 +1,279 @@
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+import {
+  AddedValidator as AddedValidatorEvent,
+  AdminChanged as AdminChangedEvent,
+  BeaconUpgraded as BeaconUpgradedEvent,
+  Burned as BurnedEvent,
+  ChangedProtocolFee as ChangedProtocolFeeEvent,
+  ChangedValidatorIndex as ChangedValidatorIndexEvent,
+  Deposited as DepositedEvent,
+  DisabledSafeguard as DisabledSafeguardEvent,
+  DistributedProtocolFee as DistributedProtocolFeeEvent,
+  Initialized as InitializedEvent,
+  NewSystemStakeLimit as NewSystemStakeLimitEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Paused as PausedEvent,
+  ReenabledSafeguard as ReenabledSafeguardEvent,
+  RemovedValidator as RemovedValidatorEvent,
+  ReplacedValidator as ReplacedValidatorEvent,
+  Unpaused as UnpausedEvent,
+  UpdatedPriceRatio as UpdatedPriceRatioEvent,
+  Upgraded as UpgradedEvent,
+  Withdrawn as WithdrawnEvent
+} from "../generated/LiquidStakingProxy/LiquidStakingProxy"
+import {
+  Transfer as TransferEvent
+} from "../generated/StakedFuse/StakedFuse"
+import { getFusePrice, getLiquidStakingContract, getOrCreateHistory, getOrCreateLiquidStaking, getOrCreateUser, getOrCreateValidator, ZERO_ADDRESS } from "./utils"
+
+export function handleAddedValidator(event: AddedValidatorEvent): void {
+  let validator = getOrCreateValidator(event.params.validatorAdded, event.block)
+  let lsContract = getLiquidStakingContract()
+  validator.active = true
+  // validator.index = lsContract.getValidatorsLength().minus(BigInt.fromI32(1))
+  validator.save()
+}
+
+export function handleAdminChanged(event: AdminChangedEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+  ls.admin = event.params.newAdmin
+  history.admin = event.params.newAdmin
+  ls.save()
+  history.save()
+}
+
+export function handleChangedProtocolFee(event: ChangedProtocolFeeEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+  ls.protocolFeeBasis = event.params.newBasisRate
+  history.protocolFeeBasis = event.params.newBasisRate
+}
+
+export function handleChangedValidatorIndex(
+  event: ChangedValidatorIndexEvent
+): void {
+  // const lsContract = getLiquidStakingContract()
+  // let validatorA = lsContract.getValidatorAt(event.params.newIndex)
+  // let validatorB = lsContract.getValidatorAt(event.params.priorIndex)
+
+  // let validatorAEntity = getOrCreateValidator(validatorA, event.block)
+  // let validatorBEntity = getOrCreateValidator(validatorB, event.block)
+
+  // validatorAEntity.index = event.params.newIndex
+  // validatorBEntity.index = event.params.priorIndex
+
+  // validatorAEntity.save()
+  // validatorBEntity.save()
+}
+
+export function handleDisabledSafeguard(event: DisabledSafeguardEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+
+  ls.isSafeguardEnabled = false
+  history.isSafeguardEnabled = false
+
+  ls.save()
+  history.save()
+}
+
+export function handleDistributedProtocolFee(
+  event: DistributedProtocolFeeEvent
+): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+  let fusePrice = getFusePrice()
+  let feeBasis = ls.protocolFeeBasis
+  let fees = event.params.protocolFees.toBigDecimal()
+  let rewardAmount = fees.times(BigDecimal.fromString("10000")).div(feeBasis.toBigDecimal())
+
+  ls.totalFees = ls.totalFees.plus(fees)
+  ls.totalFeesUSD = ls.totalFeesUSD.plus(fees.times(fusePrice))
+  
+  history.totalFees = ls.totalFees
+  history.totalFeesUSD = ls.totalFeesUSD
+
+  history.adminFeesDay = history.adminFeesDay.plus(fees)
+  history.adminFeesDayUSD = history.adminFeesDayUSD.plus(fees.times(fusePrice))
+
+  ls.totalRewards = ls.totalRewards.plus(rewardAmount)
+  ls.totalRewardsUSD = ls.totalRewardsUSD.plus(rewardAmount.times(fusePrice))
+
+  history.totalRewards = ls.totalRewards
+  history.totalRewardsUSD = ls.totalRewardsUSD
+
+  history.fuseRewardsDay = history.fuseRewardsDay.plus(rewardAmount)
+  history.fuseRewardsDayUSD = history.fuseRewardsDayUSD.plus(rewardAmount.times(fusePrice))
+
+  ls.save()
+  history.save()
+}
+
+export function handleNewSystemStakeLimit(
+  event: NewSystemStakeLimitEvent
+): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+
+  ls.systemStakeLimit = event.params.newLimit
+  history.systemStakeLimit = event.params.newLimit
+
+  ls.save()
+  history.save()
+}
+
+export function handlePaused(event: PausedEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+
+  ls.isPaused = true
+  history.isPaused = true
+
+  ls.save()
+  history.save()
+}
+
+export function handleReenabledSafeguard(event: ReenabledSafeguardEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+
+  ls.isSafeguardEnabled = true
+  history.isSafeguardEnabled = true
+
+  ls.save()
+  history.save()
+}
+
+export function handleRemovedValidator(event: RemovedValidatorEvent): void {
+  let validator = getOrCreateValidator(event.params.validatorRemoved, event.block)
+  validator.active = false
+  validator.save()
+}
+
+export function handleReplacedValidator(event: ReplacedValidatorEvent): void {
+  let oldValidator = getOrCreateValidator(event.params.oldValidator, event.block)
+  let newValidator = getOrCreateValidator(event.params.newValidator, event.block)
+
+  oldValidator.active = false
+  newValidator.active = true
+
+  newValidator.totalFuseDelegated = oldValidator.totalFuseDelegated
+
+  oldValidator.save()
+  newValidator.save()
+}
+
+export function handleUnpaused(event: UnpausedEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+
+  ls.isPaused = false
+  history.isPaused = false
+
+  ls.save()
+  history.save()
+}
+
+export function handleUpdatedPriceRatio(event: UpdatedPriceRatioEvent): void {
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+
+  ls.ratio = event.params.updatedRatio
+  history.ratio = event.params.updatedRatio
+
+  ls.save()
+  history.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let from = event.params.from
+  let to = event.params.to
+
+  if (from.toHexString() == ZERO_ADDRESS || to.toHexString() == ZERO_ADDRESS) {
+    // Skip, already handled with Withdraw and Deposit
+    return
+  }
+
+  let ls = getOrCreateLiquidStaking()
+
+  let ratio = ls.ratio
+  let sFuseAmount = event.params.value.toBigDecimal()
+  let fuseAmount = sFuseAmount.times(ratio.toBigDecimal())
+
+  let userA = getOrCreateUser(from, event.block)
+  let userB = getOrCreateUser(to, event.block)
+
+  userA.stakedFuseBalance = userA.stakedFuseBalance.minus(sFuseAmount)
+  userB.stakedFuseBalance = userB.stakedFuseBalance.plus(sFuseAmount)
+
+  userA.fuseExited = userA.fuseExited.plus(fuseAmount)
+  userB.fuseEntered = userB.fuseEntered.plus(fuseAmount)
+
+  userA.save()
+  userB.save()
+}
+
+export function handleDeposited(event: DepositedEvent): void {
+  let user = getOrCreateUser(event.params.user, event.block)
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+  let fusePrice = getFusePrice()
+  
+  let sFuseAmount = event.params.tokens.toBigDecimal()
+  let fuseAmount = event.params.deposit.toBigDecimal()
+
+  user.stakedFuseBalance = user.stakedFuseBalance.plus(sFuseAmount)
+  user.fuseEntered = user.fuseEntered.plus(fuseAmount)
+
+  ls.totalStaked = ls.totalStaked.plus(fuseAmount)
+  ls.totalFeesUSD = ls.totalFeesUSD.plus(fuseAmount.times(fusePrice))
+
+  history.totalStaked = ls.totalStaked
+  history.totalFeesUSD = ls.totalFeesUSD
+
+  ls.sFuseMinted = ls.sFuseMinted.plus(sFuseAmount)
+  history.sFuseMinted = ls.sFuseMinted
+
+  ls.fuseEntered = ls.fuseEntered.plus(fuseAmount)
+  history.fuseEntered = ls.fuseEntered
+
+  history.fuseStakedDay = history.fuseStakedDay.plus(fuseAmount)
+  history.fuseStakedDayUSD = history.fuseStakedDayUSD.plus(fuseAmount.times(fusePrice))
+
+  user.save()
+  ls.save()
+  history.save()
+}
+
+export function handleWithdrawn(event: WithdrawnEvent): void {
+  let user = getOrCreateUser(event.params.user, event.block)
+  let ls = getOrCreateLiquidStaking()
+  let history = getOrCreateHistory(event.block)
+  let fusePrice = getFusePrice()
+
+  let sFuseAmount = event.params.tokens.toBigDecimal()
+  let fuseAmount = event.params.payout.toBigDecimal()
+
+  user.stakedFuseBalance = user.stakedFuseBalance.minus(sFuseAmount)
+  user.fuseEntered = user.fuseEntered.minus(fuseAmount)
+
+  ls.totalStaked = ls.totalStaked.minus(fuseAmount)
+  ls.totalFeesUSD = ls.totalFeesUSD.minus(fuseAmount.times(fusePrice))
+
+  history.totalStaked = ls.totalStaked
+  history.totalFeesUSD = ls.totalFeesUSD
+
+  ls.sFuseBurned = ls.sFuseBurned.plus(sFuseAmount)
+  history.sFuseBurned = ls.sFuseBurned
+
+  ls.fuseExited = ls.fuseExited.plus(fuseAmount)
+  history.fuseExited = ls.fuseExited
+
+  history.fuseUnstakedDay = history.fuseUnstakedDay.plus(fuseAmount)
+  history.fuseUnstakedDayUSD = history.fuseUnstakedDayUSD.plus(fuseAmount.times(fusePrice))
+
+  user.save()
+  ls.save()
+  history.save()
+}
---
+++ src/utils.ts
@@ -0,0 +1,228 @@
+import {
+    LiquidStaking,
+    Validator,
+    User,
+    History,
+    StakedFuse
+} from "../generated/schema"
+import {
+    LiquidStakingProxy as LiquidStakingProxyContract
+} from "../generated/LiquidStakingProxy/LiquidStakingProxy"
+import {
+    StakedFuse as StakedFuseContract
+} from "../generated/StakedFuse/StakedFuse"
+import { Address, BigDecimal, BigInt, dataSource, ethereum, log } from '@graphprotocol/graph-ts'
+import { VoltagePair as PairContract } from "../generated/LiquidStakingProxy/VoltagePair"
+
+const LIQUID_STAKING_ADDRESS = "0xa3dc222eC847Aac61FB6910496295bF344Ea46be".toLowerCase()
+const FUSE_BUSD_PAIR_ADDRESS = Address.fromHexString("0x91520fc2942fd52949514f159aa4927b8850178d")
+const BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
+const BIG_DECIMAL_1E18 = BigDecimal.fromString('1000000000000000000')
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export function getLiquidStakingContract() : LiquidStakingProxyContract {
+    return LiquidStakingProxyContract.bind(Address.fromString(LIQUID_STAKING_ADDRESS))
+}
+
+export function getFusePrice(): BigDecimal {
+    const pair = PairContract.bind(Address.fromBytes(FUSE_BUSD_PAIR_ADDRESS))
+    const reservesResult = pair.try_getReserves()
+    if (reservesResult.reverted) {
+        log.info('[getJoePrice] getReserves reverted', [])
+        return BIG_DECIMAL_ZERO
+    }
+    const reserves = reservesResult.value
+    if (reserves.value0.toBigDecimal().equals(BigDecimal.fromString('0'))) {
+        log.error('[getJoePrice] USDC reserve 0', [])
+        return BIG_DECIMAL_ZERO
+    }
+    return reserves.value1.toBigDecimal().times(BIG_DECIMAL_1E18).div(reserves.value0.toBigDecimal()).div(BIG_DECIMAL_1E18)
+}
+
+function getOrCreateStakedFuse(sfAddress: Address): StakedFuse {
+    let sf = StakedFuse.load(sfAddress.toHex())
+    let sfContract = StakedFuseContract.bind(sfAddress)
+    if (sf == null) {
+        sf = new StakedFuse(sfAddress.toHex())
+        sf.address = sfAddress
+        sf.decimals = sfContract.decimals()
+        sf.symbol = sfContract.symbol()
+        sf.name = sfContract.name()
+        sf.totalSupply = sfContract.totalSupply()
+    }
+    sf.save()
+    return sf
+}
+
+function createLiquidStaking(block: ethereum.Block): LiquidStaking {
+    let ls = new LiquidStaking(LIQUID_STAKING_ADDRESS)
+    let liquidStakingContract = getLiquidStakingContract()
+    let sf = getOrCreateStakedFuse(liquidStakingContract.getSFToken())
+
+    ls.sFuse = sf.id
+    ls.ratio = liquidStakingContract.priceRatio()
+
+    ls.totalStaked = BIG_DECIMAL_ZERO
+    ls.totalStakedUSD = BIG_DECIMAL_ZERO
+
+    // We assume the contract is empty
+    ls.sFuseMinted = BIG_DECIMAL_ZERO
+    ls.sFuseBurned = BIG_DECIMAL_ZERO
+
+    ls.totalRewards = BIG_DECIMAL_ZERO
+    ls.totalRewardsUSD = BIG_DECIMAL_ZERO
+
+    ls.totalFees = BIG_DECIMAL_ZERO
+    ls.totalFeesUSD = BIG_DECIMAL_ZERO
+
+    ls.fuseEntered = BIG_DECIMAL_ZERO
+    ls.fuseEnteredUSD = BIG_DECIMAL_ZERO
+
+    ls.fuseExited = BIG_DECIMAL_ZERO
+    ls.fuseExitedUSD = BIG_DECIMAL_ZERO
+
+    ls.updatedAt = block.timestamp
+
+    ls.usersCount = BigInt.fromI32(0)
+    ls.validatorsCount = BigInt.fromI32(0)
+    
+    ls.protocolFeeBasis = liquidStakingContract.getProtocolFeeBasis()
+
+    ls.isPaused = liquidStakingContract.isPaused()
+    ls.isSafeguardEnabled = liquidStakingContract.isSafeguardLimitEnabled()
+    
+    ls.admin = liquidStakingContract.owner()
+
+    ls.systemStakeLimit = liquidStakingContract.systemStakeLimit()
+
+    ls.save()
+    return ls
+}
+
+export function getOrCreateLiquidStaking(block: ethereum.Block | null = null): LiquidStaking {
+    let liquidStaking = LiquidStaking.load(LIQUID_STAKING_ADDRESS)
+    if (liquidStaking == null && block != null) {
+        liquidStaking = createLiquidStaking(block)
+    }
+    return liquidStaking as LiquidStaking
+}
+
+function getDay(timestamp: BigInt): BigInt {
+    return timestamp.div(BigInt.fromI32(86400))
+}
+
+function createHistory(day: BigInt): History {
+    let history = new History(day.toString())
+    let ls = getOrCreateLiquidStaking()
+    
+    history.date = day
+    history.sFuse = ls.sFuse
+    history.ratio = ls.ratio
+    history.totalStaked = ls.totalStaked
+    history.totalStakedUSD = ls.totalStakedUSD
+
+    history.sFuseMinted = ls.sFuseMinted
+    history.sFuseBurned = ls.sFuseBurned
+
+    history.totalRewards = ls.totalRewards
+    history.totalRewardsUSD = ls.totalRewardsUSD
+
+    history.totalFees = ls.totalFees
+    history.totalFeesUSD = ls.totalFeesUSD
+
+    history.fuseEntered = ls.fuseEntered
+    history.fuseEnteredUSD = ls.fuseEnteredUSD
+
+    history.fuseExited = ls.fuseExited
+    history.fuseExitedUSD = ls.fuseExitedUSD
+
+    history.usersCount = ls.usersCount
+    history.validatorsCount = ls.validatorsCount
+
+    history.protocolFeeBasis = ls.protocolFeeBasis
+    history.isPaused = ls.isPaused
+    history.isSafeguardEnabled = ls.isSafeguardEnabled
+
+    return history
+
+}
+
+export function getOrCreateHistory(block: ethereum.Block): History {
+    let day = getDay(block.timestamp)
+    let history = History.load(day.toString())
+    if (history == null) {
+        history = createHistory(day)
+        history.fuseStakedDay = BIG_DECIMAL_ZERO
+        history.fuseStakedDayUSD = BIG_DECIMAL_ZERO
+        history.fuseUnstakedDay = BIG_DECIMAL_ZERO
+        history.fuseUnstakedDayUSD = BIG_DECIMAL_ZERO
+
+        history.fuseRewardsDay = BIG_DECIMAL_ZERO
+        history.fuseRewardsDayUSD = BIG_DECIMAL_ZERO
+        history.adminFeesDay = BIG_DECIMAL_ZERO
+        history.adminFeesDayUSD = BIG_DECIMAL_ZERO
+    }
+
+    let ls = getOrCreateLiquidStaking()
+    
+    history.date = day
+    history.sFuse = ls.sFuse
+    history.ratio = ls.ratio
+    history.totalStaked = ls.totalStaked
+    history.totalStakedUSD = ls.totalStakedUSD
+
+    history.sFuseMinted = ls.sFuseMinted
+    history.sFuseBurned = ls.sFuseBurned
+
+    history.totalRewards = ls.totalRewards
+    history.totalRewardsUSD = ls.totalRewardsUSD
+
+    history.totalFees = ls.totalFees
+    history.totalFeesUSD = ls.totalFeesUSD
+
+    history.fuseEntered = ls.fuseEntered
+    history.fuseEnteredUSD = ls.fuseEnteredUSD
+
+    history.fuseExited = ls.fuseExited
+    history.fuseExitedUSD = ls.fuseExitedUSD
+
+    history.usersCount = ls.usersCount
+    history.validatorsCount = ls.validatorsCount
+
+    history.protocolFeeBasis = ls.protocolFeeBasis
+    history.isPaused = ls.isPaused
+    history.isSafeguardEnabled = ls.isSafeguardEnabled
+
+    history.save()
+    return history as History
+}
+
+export function getOrCreateValidator(address: Address, block: ethereum.Block): Validator {
+    let validator = Validator.load(address.toHexString())
+    if (validator == null) {
+        validator = new Validator(address.toHexString())
+        let liquidStaking = getOrCreateLiquidStaking(block)
+        validator.addedAt = block.timestamp
+        validator.liquidStaking = liquidStaking.id
+        validator.totalFuseDelegated = BIG_DECIMAL_ZERO
+        validator.active = false
+        validator.save()
+    }
+    return validator
+}
+
+export function getOrCreateUser(address: Address, block: ethereum.Block): User {
+    let user = User.load(address.toHexString())
+
+    if( user == null ) {
+        user = new User(address.toHexString())
+        let liquidStaking = getOrCreateLiquidStaking(block)
+        user.liquidStaking = liquidStaking.id
+        user.stakedFuseBalance = BIG_DECIMAL_ZERO
+        user.fuseEntered = BIG_DECIMAL_ZERO
+        user.fuseExited = BIG_DECIMAL_ZERO
+        user.save()
+    }
+
+    return user
+}
